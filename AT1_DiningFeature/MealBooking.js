08a865619e505518a6bbb2b6c2d60c47c038e909
eaumal123 <eauthesamallen@gmail.com>
2026-07-20 20:37:48 +1000

    Adding getters and setters

--- AT1_DiningFeature/MealBooking.js
+++ AT1_DiningFeature/MealBooking.js
@@ -16,5 +16,68 @@ class MealBooking {
         this.#dietaryNote = dietaryNote;
         this.#bookingStatus = "Pending";
     }
+
+    //Create appropriate getters and setters
+    
+    // Student ID
+    get studentId() {
+        return this.#studentId;
+    }
+    set studentId(value) {
+        this.#studentId = value;
+    }
+
+    // Student Name
+    get studentName() {
+        return this.#studentName;
+    }
+    set studentName(value) {
+        this.#studentName = value;
+    }
+
+    // Meal Date
+    get mealDate() {
+        return this.#mealDate;
+    }
+    set mealDate(value) {
+        this.#mealDate = value;
+    }
+
+    // Meal Type
+    get mealType() {
+        return this.#mealType;
+    }
+    set mealType(value) {
+        this.#mealType = value;
+    }
+
+    // Quantity
+    get quantity() {
+        return this.#quantity;
+    }
+    set quantity(value) {
+        if (value > 0) {
+            this.#quantity = value;
+        } else {
+            console.log("Quantity must be greater than 0.");
+        }
+    }
+
+    // Dietary Note
+    get dietaryNote() {
+        return this.#dietaryNote;
+    }
+    set dietaryNote(value) {
+        this.#dietaryNote = value;
+    }
+
+    // Booking Status
+    get bookingStatus() {
+        return this.#bookingStatus;
+    }
+    set bookingStatus(value) {
+        this.#bookingStatus = value;
+    }
+
 }
 module.exports = MealBooking;
